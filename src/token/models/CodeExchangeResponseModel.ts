ddcd29f45387fd503403d45c5eba53411b9691a8
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-08-28 20:55:52 +0200

    Small changes in swagger

--- src/token/models/CodeExchangeResponseModel.ts
+++ src/token/models/CodeExchangeResponseModel.ts
@@ -1,12 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+class User {
+    @ApiProperty()
+    email: string;
+
+    @ApiProperty()
+    nick: string;
+}
+
 export class CodeExchangeResponseModel {
     @ApiProperty()
     access_token: string;
 
     @ApiProperty()
-    user: {
-        email: string;
-        nick: string;
-    };
+    user: User;
 }
