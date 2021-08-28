504e2a8890b6a348c85a408a2bf5e36f8cbb5081
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-08-28 23:26:35 +0200

    Connecting to secret store

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,41 +1,49 @@
 import { HttpService } from '@nestjs/axios';
 import { HttpException, Injectable } from '@nestjs/common';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, concatMap } from 'rxjs/operators';
 import { Http as httpStatus } from '@status/codes';
 
+import { DaprClientCredentialsDto } from './dto/DaprClientCredentialsDto';
 import { Auth0TokenRequestDto } from './dto/Auth0TokenRequestDto';
-import { CodeExchangeResponseModel } from './models/CodeExchangeResponseModel';
 
 @Injectable()
 export class TokenService {
     constructor(private http: HttpService) {}
 
     codeExchange(code: string) {
-        const data = {
-            grant_type: 'authorization_code',
-            client_id: 'ixIHAKVHg7ksQveRimdvsphtOdkVAbSh',
-            client_secret: '5cb_dwwLl6GPNw1ohoCGjkqeFbBRrz_V-KtjtqOTn8h2Gfh5Cb9CBL0WTq1YU9Iw',
-            code,
-            redirect_uri: 'http://localhost:3000',
-        };
+        return this.http
+            .get<DaprClientCredentialsDto>('http://localhost:3500/v1.0/secrets/auth-api/clientCredentials')
+            .pipe(
+                concatMap((response) => {
+                    const [clientId, clientSecret] = response.data.clientCredentials.split('.');
 
-        return this.http.post<Auth0TokenRequestDto>('https://dev-9gntu7bd.eu.auth0.com/oauth/token', data).pipe(
-            map((response): CodeExchangeResponseModel => {
-                const { id_token, access_token } = response.data;
+                    return this.http.post<Auth0TokenRequestDto>('https://dev-9gntu7bd.eu.auth0.com/oauth/token', {
+                        grant_type: 'authorization_code',
+                        client_id: clientId,
+                        client_secret: clientSecret,
+                        code,
+                        redirect_uri: 'http://localhost:3000',
+                    });
+                }),
+                map((response) => {
+                    const { id_token, access_token } = response.data;
 
-                const tokenPayload = JSON.parse(Buffer.from(id_token.split('.')[1], 'base64').toString());
+                    const tokenPayload = JSON.parse(Buffer.from(id_token.split('.')[1], 'base64').toString());
 
-                return { access_token, user: { email: tokenPayload.email, nick: tokenPayload.nickname } };
-            }),
-            catchError((err) => {
-                const { error = null } = err.response && err.response.data ? err.response.data : {};
+                    return {
+                        access_token,
+                        user: { email: tokenPayload.email, nick: tokenPayload.nickname },
+                    };
+                }),
+                catchError((err) => {
+                    const { error = null } = err.response && err.response.data ? err.response.data : {};
 
-                if (error === 'invalid_grant') {
-                    throw new HttpException('Invalid code', httpStatus.Forbidden);
-                }
+                    if (error === 'invalid_grant') {
+                        throw new HttpException('Invalid code', httpStatus.Forbidden);
+                    }
 
-                throw err;
-            }),
-        );
+                    throw err;
+                }),
+            );
     }
 }
